d8e1a9f53c5c10c9280aab5ec03099f8579e049f
suzumiya <yangjinzhu@iie.ac.cn>
2015-12-12 00:17:52 +0800

    simple validate

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+validates :name,  presence: true, length: { maximum: 15, minimum: 2}
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+end
