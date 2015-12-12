22853ff4b874dddb36720009ea4d0fe70cc8d921
suzumiya <yangjinzhu@iie.ac.cn>
2015-12-12 09:37:44 +0800

    finish pwd validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ validates :email, presence: true, length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+
+validates :password, presence: true, length: { minimum: 4 }
 has_secure_password
 end
